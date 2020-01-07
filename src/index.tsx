84b8443ee44334ef343cde979867a6877008c9b9
Sandu Victor <vyctor661@gmail.com>
2020-01-07 15:15:59 +0200

    moved files in the web folder

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import App from "./web/components/App";
-
-ReactDOM.render(<App color="Yellow" />, document.getElementById("root"));
