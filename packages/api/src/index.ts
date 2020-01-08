73906b121b420215d3ad4c7040e8f6f602c90d76
Sandu Victor <vyctor661@gmail.com>
2020-01-09 01:23:44 +0200

    solved nodemon bug when running dev script

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,9 +1,11 @@
-import koa from "koa"
+import koa, {Context} from "koa"
 
 const app = new koa()
 
-app.use(async context => {
+app.use(async (context: Context ) => {
     context.body = "Hello world!"
 })
 
-app.listen(8090)
+app.listen(8090, ()=>{
+    console.log('something');
+})
