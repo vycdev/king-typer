93926dbd516fb78432485daeaeec119d94d6cefa
Sandu Victor <vyctor661@gmail.com>
2020-01-12 19:32:35 +0200

    testing emotion and react together

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -3,4 +3,29 @@ import * as ReactDOM from "react-dom"
 
 import App from "./components/App"
 
-ReactDOM.render(<App color="Yellow" />, document.getElementById("root"))
+import styled from "@emotion/styled"
+
+const Apps = styled.div`
+    background-color: blue;
+`
+
+const Anotherone = styled("div")`
+    background-color: yellow;
+`
+
+class Apppp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Apps>
+                    <App color="blue"></App>
+                </Apps>
+                <Anotherone>
+                    <App color="Ye dude imagine that being yellow"></App>
+                </Anotherone>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Apppp />, document.getElementById("root"))
