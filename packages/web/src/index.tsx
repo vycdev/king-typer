8b4251d689c1a0c695f1bb547c4838a6392bd5d5
Sandu Victor <vyctor661@gmail.com>
2020-02-13 19:19:03 +0200

    refactor:Removed some unnecessary code

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,33 +1,10 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
-import App from "./components/App"
 import { Box } from "./components/typingBox/typingBox"
 
-import styled from "@emotion/styled"
-
-const Apps = styled.div`
-    background-color: blue;
-`
-
-const Anotherone = styled("div")`
-    background-color: yellow;
-`
-
-const Apppp = () => {
-    return (
-        <>
-            <div>
-                <Apps>
-                    <App text="blue"></App>
-                </Apps>
-                <Anotherone>
-                    <App text="Ye dude imagine that being yellow"></App>
-                </Anotherone>
-                <Box></Box>
-            </div>
-        </>
-    )
+const App = () => {
+    return <Box></Box>
 }
 
-ReactDOM.render(<Apppp />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
