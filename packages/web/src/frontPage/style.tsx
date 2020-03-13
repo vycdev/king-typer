fe06bdaf9c40534efdb3e4c3385454c2eec0ac5d
Sandu Victor <vyctor661@gmail.com>
2020-03-13 20:32:16 +0200

    style: made title responsive

--- packages/web/src/frontPage/style.tsx
+++ packages/web/src/frontPage/style.tsx
@@ -2,9 +2,9 @@ import styled from "@emotion/styled";
 //// CONTAINER 1
 export const Container = styled.div`
     background-color: #198cf6;
-    height: min(900px, 1200px);
+    height: min(930px, 1444px);
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(420px, 1fr));
     width: 100%;
 
     &:last-child {
@@ -17,6 +17,7 @@ export const SvgCrown = styled.div`
 `;
 export const Fix = styled.div`
     position: relative;
+    width: 100%;
 `;
 
 export const VerticallyCenteredDiv = styled.div`
@@ -29,5 +30,23 @@ export const VerticallyCenteredDiv = styled.div`
 `;
 //// TEXT ON THE RIGHT 1
 export const TitleTextButton = styled.div`
+    @import url("https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap");
+    font-family: "Press Start 2P", cursive;
+    color: white;
+    width: 100%;
     text-align: center;
+`;
+export const VerticallyCenteredText = styled.div`
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    width: 100%;
+`;
+
+export const Title = styled.div`
+    font-size: 70px;
+    width: auto;
 `;
