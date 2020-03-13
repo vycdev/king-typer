ac56e968465b6dddd4115c14be3425e729aab83e
Sandu Victor <vyctor661@gmail.com>
2020-03-13 12:43:39 +0200

    chore: commas everywhere

--- packages/web/src/server.ts
+++ packages/web/src/server.ts
@@ -1,15 +1,15 @@
-import koa from 'koa';
-import serve from 'koa-static';
+import koa from "koa";
+import serve from "koa-static";
 
-import path from 'path';
+import path from "path";
 
 const app = new koa();
-const serverPath = path.join(__dirname)
+const serverPath = path.join(__dirname);
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.use(serve(serverPath));
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Wep app started on port ${port}`);
 });
