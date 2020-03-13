73e8cbb752b9b4d051be5e0118be33ef49302fa8
Sandu Victor <vyctor661@gmail.com>
2020-03-13 18:45:46 +0200

    style: made first pannel responsive

--- packages/web/src/frontPage/style.tsx
+++ packages/web/src/frontPage/style.tsx
@@ -1,6 +1,33 @@
 import styled from "@emotion/styled";
+//// CONTAINER 1
+export const Container = styled.div`
+    background-color: #198cf6;
+    height: min(900px, 1200px);
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+    width: 100%;
 
+    &:last-child {
+        grid-column: span 2;
+    }
+`;
+//// CROWN SVG 1
 export const SvgCrown = styled.div`
-    height: 261px;
-    width: 200px;
+    width: 400px;
+`;
+export const Fix = styled.div`
+    position: relative;
+`;
+
+export const VerticallyCenteredDiv = styled.div`
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+`;
+//// TEXT ON THE RIGHT 1
+export const TitleTextButton = styled.div`
+    text-align: center;
 `;
