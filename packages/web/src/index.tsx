6bdfd0405ca58b6977924ada80e0656d8653c52e
Sandu Victor <vyctor661@gmail.com>
2020-03-13 13:31:54 +0200

    style: simple fix for body style

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,26 +1,24 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import * as ReactDOM from "react-dom";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import { Home } from "./frontPage/home";
+import { Navbar } from "./components/navbar/navBar";
 
 const App = () => {
+    useEffect(() => {
+        document.body.style.margin = "0";
+    });
     return (
-        <>
-            <Router>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                </ul>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </Router>
-        </>
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     );
 };
 ReactDOM.render(<App />, document.getElementById("root"));
