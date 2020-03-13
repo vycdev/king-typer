312fc7766f3f7babc462af6c58b6a002e4b5f655
Sandu Victor <vyctor661@gmail.com>
2020-03-13 12:02:00 +0200

    chore: semicolumns everywhere

--- packages/web/src/components/typingBox/typingBox.tsx
+++ packages/web/src/components/typingBox/typingBox.tsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 
-import { Wrapper, Container, Top, Text, Bottom, LetterElement } from "./style"
+import { Wrapper, Container, Top, Text, Bottom, LetterElement } from "./style";
 
-let string = ""
-let increment = 0
-let color = "green"
-let stringFullyBackspaced = false
-let charTyped = 0
-let correctedCharTyped = 0
-let CPM = 0
-let WPM = 0
+let string = "";
+let increment = 0;
+let color = "green";
+let stringFullyBackspaced = false;
+let charTyped = 0;
+let correctedCharTyped = 0;
+let CPM = 0;
+let WPM = 0;
 
 const getText = () => {
-    return "A (from the Ancient Greek paragraphos, \"to write beside\" or \"written beside\") is a self-contained unit of a discourse in writing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose."
-}
+    return "A (from the Ancient Greek paragraphos, \"to write beside\" or \"written beside\") is a self-contained unit of a discourse in writing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.";
+};
 
 const checkBackspaceReturn = (e: any) => {
     onKey(
@@ -25,8 +25,8 @@ const checkBackspaceReturn = (e: any) => {
             : e.keyCode === 32
             ? "space"
             : "undefined"
-    )
-}
+    );
+};
 const onKey = (e: any, special: string = "undefined") => {
     if (
         window.event &&
@@ -34,23 +34,23 @@ const onKey = (e: any, special: string = "undefined") => {
         String.fromCharCode(e.charCode) != " " &&
         String.fromCharCode(e.charCode) != ""
     ) {
-        string += String.fromCharCode(e.charCode)
-        stringFullyBackspaced = false
+        string += String.fromCharCode(e.charCode);
+        stringFullyBackspaced = false;
     } else if (special == "backspace") {
-        string = string.slice(0, -1)
+        string = string.slice(0, -1);
         if (string.length === 0) {
-            stringFullyBackspaced = true
+            stringFullyBackspaced = true;
         }
     } else if (special == "enter") {
-        checkAndIncrement()
+        checkAndIncrement();
     } else if (special == "space") {
-        checkAndIncrement()
+        checkAndIncrement();
     }
-    return string
-}
+    return string;
+};
 
 const buildTextComponentsArray = (text: string, elColor: string) => {
-    const splittedText = text.split(" ")
+    const splittedText = text.split(" ");
 
     return splittedText.map((word: string, index) => {
         if (index <= increment) {
@@ -58,9 +58,9 @@ const buildTextComponentsArray = (text: string, elColor: string) => {
                 return splittedText[increment]
                     .split("")
                     .map((letter: string, i) => {
-                        let addSpace = ""
+                        let addSpace = "";
                         if (word.split("").length - 1 === i) {
-                            addSpace = " "
+                            addSpace = " ";
                         }
 
                         if (
@@ -80,7 +80,7 @@ const buildTextComponentsArray = (text: string, elColor: string) => {
                                     <u>{letter}</u>
                                     {addSpace}
                                 </LetterElement>
-                            )
+                            );
                         } else {
                             if (
                                 i <= string.split("").length - 1 &&
@@ -94,10 +94,10 @@ const buildTextComponentsArray = (text: string, elColor: string) => {
                                         <u>{letter}</u>
                                         {addSpace}
                                     </LetterElement>
-                                )
+                                );
                             } else {
                                 if (stringFullyBackspaced) {
-                                    stringFullyBackspaced = false
+                                    stringFullyBackspaced = false;
                                 }
 
                                 return (
@@ -108,81 +108,81 @@ const buildTextComponentsArray = (text: string, elColor: string) => {
                                         <u>{letter}</u>
                                         {addSpace}
                                     </LetterElement>
-                                )
+                                );
                             }
                         }
-                    })
+                    });
             } else {
                 return (
                     <LetterElement key={index} style={{ color: elColor }}>
                         {word}{" "}
                     </LetterElement>
-                )
+                );
             }
         }
         return (
             <LetterElement key={index} style={{ color: "#0752e8" }}>
                 {word}{" "}
             </LetterElement>
-        )
-    })
-}
+        );
+    });
+};
 
 const getTextToBeCompared = (text: string) => {
-    return text.split(" ")
-}
+    return text.split(" ");
+};
 
 const checkAndIncrement = () => {
     if (string === getTextToBeCompared(getText())[increment]) {
-        increment++
-        charTyped += string.length + 1
-        correctedCharTyped += string.length + 1
-        color = "green"
-        string = ""
+        increment++;
+        charTyped += string.length + 1;
+        correctedCharTyped += string.length + 1;
+        color = "green";
+        string = "";
     } else {
-        correctedCharTyped += string.length + 1
+        correctedCharTyped += string.length + 1;
         console.log(
             string,
             increment,
             getTextToBeCompared(getText())[increment]
-        )
+        );
     }
-}
+};
 
 const setCpmWpm = (cpm: number, wpm: number) => {
-    CPM = cpm
-    WPM = wpm
-}
+    CPM = cpm;
+    WPM = wpm;
+};
 
 const Input = () => {
-    const [inner, changeInner] = useState("")
+    const [inner, changeInner] = useState("");
 
     return (
         <Bottom
             value={inner}
             onKeyPress={e => changeInner(onKey(e))}
             onKeyDown={e => {
-                checkBackspaceReturn(e)
-                changeInner(string)
+                checkBackspaceReturn(e);
+                changeInner(string);
             }}
             onChange={() => {}}
             autoFocus
         ></Bottom>
-    )
-}
+    );
+};
 
 export const Box = () => {
     const [text, updateText] = useState(
         buildTextComponentsArray(getText(), color)
-    )
-    const [time, setTime] = useState(60)
-    const [cpm, setCpm] = useState(0)
-    const [wpm, setWpm] = useState(0)
+    );
+    const [time, setTime] = useState(60);
+    const [cpm, setCpm] = useState(0);
+    const [wpm, setWpm] = useState(0);
 
     if (time === 0) {
-        setCpmWpm(cpm, wpm)
+        setCpmWpm(cpm, wpm);
     } else if (time === 60 && cpm === 0 && CPM === 0) {
-        setCpmWpm(-1, 0)
+        setCpmWpm(-1, 0);
     }
     const calculateCpmWpm = () => {
         return time === 0
@@ -194,18 +194,18 @@ export const Box = () => {
                   Math.floor(
                       charTyped / 5 / ((60 - time === 0 ? 1 : 60 - time) / 60)
                   )
-              ]
-    }
+              ];
+    };
     const resetEverything = () => {
-        string = ""
-        increment = 0
-        color = "green"
-        stringFullyBackspaced = false
-        charTyped = 0
-        correctedCharTyped = 0
-        CPM = 0
-        WPM = 0
-    }
+        string = "";
+        increment = 0;
+        color = "green";
+        stringFullyBackspaced = false;
+        charTyped = 0;
+        correctedCharTyped = 0;
+        CPM = 0;
+        WPM = 0;
+    };
 
     useEffect(() => {
         setTimeout(() => {
@@ -215,20 +215,20 @@ export const Box = () => {
                     : CPM === -1 && cpm === 0
                     ? 60
                     : time - 1
-            )
-            setCpm(calculateCpmWpm()[0])
-            setWpm(calculateCpmWpm()[1])
-        }, 1000)
-    })
+            );
+            setCpm(calculateCpmWpm()[0]);
+            setWpm(calculateCpmWpm()[1]);
+        }, 1000);
+    });
 
     return (
         <Wrapper>
             <Container
                 onKeyDown={() => {
-                    updateText(buildTextComponentsArray(getText(), color))
+                    updateText(buildTextComponentsArray(getText(), color));
                 }}
                 onKeyPress={() => {
-                    updateText(buildTextComponentsArray(getText(), color))
+                    updateText(buildTextComponentsArray(getText(), color));
                 }}
             >
                 <Top>
@@ -251,13 +251,13 @@ export const Box = () => {
                                 height: "20px"
                             }}
                             onClick={() => {
-                                resetEverything()
-                                setTime(60)
-                                setCpm(0)
-                                setWpm(0)
+                                resetEverything();
+                                setTime(60);
+                                setCpm(0);
+                                setWpm(0);
                                 updateText(
                                     buildTextComponentsArray(getText(), color)
-                                )
+                                );
                             }}
                         >
                             {"Try again"}
@@ -268,5 +268,5 @@ export const Box = () => {
                 <Input></Input>
             </Container>
         </Wrapper>
-    )
-}
+    );
+};
