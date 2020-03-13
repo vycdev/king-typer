1dc9f0ff49c9fb3d663752b0f8e351767d3d472c
Sandu Victor <vyctor661@gmail.com>
2020-03-13 20:30:25 +0200

    feat: added better global styles

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,24 +1,27 @@
-import React, { useEffect } from "react";
+import React from "react";
 import * as ReactDOM from "react-dom";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Home } from "./frontPage/home";
 import { Navbar } from "./components/navbar/navBar";
 
+import { Global } from "@emotion/core";
+import { globalStyle } from "./style";
+
 const App = () => {
-    useEffect(() => {
-        document.body.style.margin = "0";
-    });
     return (
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-            </Switch>
-        </Router>
+        <>
+            <Global styles={globalStyle} />
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
+        </>
     );
 };
 ReactDOM.render(<App />, document.getElementById("root"));
