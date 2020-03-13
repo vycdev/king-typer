85ffc7eda481012593b9ced1b7e65aff0127df63
Sandu Victor <vyctor661@gmail.com>
2020-03-13 13:32:47 +0200

    feat: added style importing from style file

--- packages/web/src/components/navbar/navBar.tsx
+++ packages/web/src/components/navbar/navBar.tsx
@@ -2,12 +2,17 @@ import * as React from "react";
 
 import { Link } from "react-router-dom";
 
+import { Ul, Li } from "./style";
+
 export const Navbar = () => {
     return (
-        <ul>
-            <li>
+        <Ul>
+            <Li>
                 <Link to="/">Home</Link>
-            </li>
-        </ul>
+            </Li>
+            <Li>
+                <Link to="/type">Start typing</Link>
+            </Li>
+        </Ul>
     );
 };
