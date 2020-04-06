a77aacab0850dd11a58d7eef983d74b6ddaaea0d
Sandu Victor <vyctor661@gmail.com>
2020-04-06 13:18:54 +0300

    feat: added pannel 2 for home page

--- packages/web/src/components/frontPage/home.tsx
+++ packages/web/src/components/frontPage/home.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 
 import { Pannel1 } from "./components/pannel1/pannel1";
+import { Pannel2 } from "./components/pannel2/pannel2";
 
 export const Home = () => {
-    return <Pannel1></Pannel1>;
+    return (
+        <>
+            <Pannel1></Pannel1>
+            <Pannel2></Pannel2>
+        </>
+    );
 };
