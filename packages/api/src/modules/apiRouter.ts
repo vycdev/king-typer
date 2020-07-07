ff10cb844fdb3e228d946553692b36d449f53ea2
xWafl <73sampleperson@gmail.com>
2020-07-07 13:33:33 -0400

    chore: updated api router

--- packages/api/src/modules/apiRouter.ts
+++ packages/api/src/modules/apiRouter.ts
@@ -2,12 +2,14 @@ import Router from "./Router";
 
 import authRouter from "./auth/router";
 import gamesRouter from "./games/router";
+import textsRouter from "./texts/router";
 import usersRouter from "./users/router";
 
 const apiRouter = new Router({ prefix: "/api" });
 
 apiRouter.use(authRouter);
 apiRouter.use(gamesRouter);
+apiRouter.use(textsRouter);
 apiRouter.use(usersRouter);
 
 export default apiRouter.routes();
