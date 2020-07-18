38673bf497d65a71250af521c26bd3a86551e668
xWafl <73sampleperson@gmail.com>
2020-07-18 08:19:01 -0400

    fix: made queue joins process queue

--- packages/api/src/modules/websockets/actions/joinQueue.ts
+++ packages/api/src/modules/websockets/actions/joinQueue.ts
@@ -1,10 +1,12 @@
 import { queue } from "../gamesData";
 import HandlerResponse from "../types/HandlerResponse";
 import WebSocket from "ws";
+import processQueue from "./processQueue";
 
 export default (data: number, ws: WebSocket): HandlerResponse => {
     const changeWSKey = Math.floor(Math.random() * 899999) + 100000;
     queue.push({ id: data, ws, changeWSKey });
+    processQueue();
     return {
         category: "joinResponse",
         data: [{ client: ws, data: { success: true, changeWSKey } }]
