eff56b30c9f41af7a775aac3cac2d66c5a667e16
xWafl <73sampleperson@gmail.com>
2020-07-24 16:57:32 -0400

    fix: users cant join queue if they are in a game

--- packages/api/src/modules/websockets/actions/joinQueue.ts
+++ packages/api/src/modules/websockets/actions/joinQueue.ts
@@ -1,4 +1,4 @@
-import { queue, Difficulty } from "../gamesData";
+import { queue, Difficulty, games } from "../gamesData";
 import HandlerResponse from "../types/HandlerResponse";
 import WebSocket from "ws";
 import processQueue from "./processQueue";
@@ -7,14 +7,22 @@ export default async (
     data: { id: number; difficulty: Difficulty },
     ws: WebSocket
 ): Promise<HandlerResponse[]> => {
+    const falseJoinResponse: HandlerResponse[] = [
+        {
+            category: "joinResponse",
+            data: [{ client: ws, data: { success: false } }]
+        }
+    ];
     const key = Math.floor(Math.random() * 899999) + 100000;
     if (queue[data.difficulty].some(l => l.id === data.id)) {
-        return [
-            {
-                category: "joinResponse",
-                data: [{ client: ws, data: { success: false } }]
-            }
-        ];
+        return falseJoinResponse;
+    }
+    if (
+        Object.values(games).some(l =>
+            l.players.map(j => j.id).includes(data.id)
+        )
+    ) {
+        return falseJoinResponse;
     }
     queue[data.difficulty].push({ id: data.id, ws, key });
     const processResp = await processQueue(data.difficulty);
